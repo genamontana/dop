0aa12d30afbf6f8a8528f38c5bca8769c6936146
genamontana <predator199@yandex.ru>
2023-02-09 10:51:43 +0300

    Dop 1 Tuesday in process

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App1 from './dop1/App1';
+import App2 from './dop2/App2';
+import App3 from './dop3/App3';
+import App4 from './dop4/App4';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/*<App/>*/}
+        <App1/>
+        {/*<App2/>*/}
+        {/*<App3/>*/}
+       {/* <App4/>*/}
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
